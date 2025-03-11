df53a24666167cd10d5bf2f8897ad1de7f5f6a9e
Tomos Evans <44647209+tomosdevans@users.noreply.github.com>
2025-03-11 00:36:32 +0000

    chore: code tidy

--- index.js
+++ index.js
@@ -1,134 +1,137 @@
-'use strict'
+"use strict";
 
-const streamToArray = require('stream-to-array');
+const streamToArray = require("stream-to-array");
 
 const baseProvider = {
-  extend (obj) {
+  extend(obj) {
     // strapi.log.info("DEBUG - extend");
     // strapi.log.info(obj)
-    Object.assign(this, obj)
+    Object.assign(this, obj);
   },
-  upload () {
-    throw new Error('Provider upload method is not implemented')
+  upload() {
+    throw new Error("Provider upload method is not implemented");
   },
-  delete () {
-    throw new Error('Provider delete method is not implemented')
-  }
-}
+  delete() {
+    throw new Error("Provider delete method is not implemented");
+  },
+};
 
 // removed reliance on strapi v3 api
 // const { convertToStrapiError } = require('../strapi-plugin-upload/errors')
 
-const wrapFunctionForErrors = fn => async (...args) => {
-  try {
-    return await fn(...args)
-  } catch (err) {
-    // throw convertToStrapiError(err)
-    strapi.log.error(err)
-    throw new Error(err)
-  }
-}
+const wrapFunctionForErrors =
+  (fn) =>
+  async (...args) => {
+    try {
+      return await fn(...args);
+    } catch (err) {
+      // throw convertToStrapiError(err)
+      strapi.log.error(err);
+      throw new Error(err);
+    }
+  };
 
 const getProviderData = (file, options) => {
   strapi.log.info("DEBUG - getProviderData");
   // strapi.log.info(file);
   strapi.log.info(JSON.stringify(options));
-  if (!options.selectProvider || typeof options.selectProvider !== 'function') {
-    const msg = `config must define a selectProvider function`
-    strapi.log.error(msg)
-    throw new Error(msg)
+  if (!options.selectProvider || typeof options.selectProvider !== "function") {
+    const msg = `config must define a selectProvider function`;
+    strapi.log.error(msg);
+    throw new Error(msg);
   }
 
-  
-  let providerKey
+  let providerKey;
   try {
-    providerKey = options.selectProvider(file)
+    providerKey = options.selectProvider(file);
     strapi.log.info(`DEBUG - selected provider is ${providerKey}`);
   } catch (err) {
-    const msg = `The function selectProvider generated error`
-    strapi.log.error(msg)
-    strapi.log.error(err)
-    throw new Error(msg)
+    const msg = `The function selectProvider generated error`;
+    strapi.log.error(msg);
+    strapi.log.error(err);
+    throw new Error(msg);
   }
 
   if (!options.providers) {
-    const msg = `You must set providers object in providerOptions of config/plugins.js`
-    strapi.log.error(msg)
-    throw new Error(msg)
+    const msg = `You must set providers object in providerOptions of config/plugins.js`;
+    strapi.log.error(msg);
+    throw new Error(msg);
   }
 
-  const p = options.providers[providerKey]
+  const p = options.providers[providerKey];
   if (!p) {
-    const msg = `The upload provider selector with key '${providerKey}' not found`
-    strapi.log.error(msg)
-    throw new Error(msg)
+    const msg = `The upload provider selector with key '${providerKey}' not found`;
+    strapi.log.error(msg);
+    throw new Error(msg);
   }
 
-  let providerInstance
+  let providerInstance;
   try {
     strapi.log.info(`DEBUG - initialising ${p.provider}`);
-    providerInstance = require(`${p.provider}`).init(
-      p.options
-    )
-    strapi.log.info(`DEBUG - provider instance ${JSON.stringify(providerInstance)}`);
+    providerInstance = require(`${p.provider}`).init(p.options);
+    strapi.log.info(
+      `DEBUG - provider instance ${JSON.stringify(providerInstance)}`
+    );
   } catch (err) {
-    const msg = `The provider package isn't installed. Please run \`npm install ${p.provider}\``
-    strapi.log.error(msg)
-    throw new Error(msg)
+    const msg = `The provider package isn't installed. Please run \`npm install ${p.provider}\``;
+    strapi.log.error(msg);
+    throw new Error(msg);
   }
 
   const providerFunctions = Object.assign(Object.create(baseProvider), {
     ...providerInstance,
-    upload: wrapFunctionForErrors(file => {
+    upload: wrapFunctionForErrors((file) => {
       strapi.log.info("DEBUG - upload");
       strapi.log.info(JSON.stringify(providerInstance));
-      return providerInstance.upload(file)
+      return providerInstance.upload(file);
     }),
     uploadStream: wrapFunctionForErrors(async (file) => {
       strapi.log.info("DEBUG - uploadStream");
       strapi.log.info("DEBUG - uploadStream", providerInstance);
       // strapi.log.info(JSON.stringify(providerInstance));
       if (providerInstance.uploadStream) {
-        return providerInstance.uploadStream(file)
+        return providerInstance.uploadStream(file);
       } else {
         // fall back on converting file stream to buffer and using existing - will break on large files
         let buffer = await streamToArray(file.stream).then(function (parts) {
-          const buffers = parts.map(part => Buffer.isBuffer(part) ? part : Buffer.from(part));
+          const buffers = parts.map((part) =>
+            Buffer.isBuffer(part) ? part : Buffer.from(part)
+          );
           return Buffer.concat(buffers);
         });
-        let fileWithBuffer = Object.assign(file, {buffer: buffer});
+        let fileWithBuffer = Object.assign(file, { buffer: buffer });
 
-        return providerInstance.upload(fileWithBuffer)
+        return providerInstance.upload(fileWithBuffer);
       }
     }),
-    delete: wrapFunctionForErrors(file => {
-      return providerInstance.delete(file)
+    delete: wrapFunctionForErrors((file) => {
+      return providerInstance.delete(file);
     }),
     isPrivate: wrapFunctionForErrors(async () => {
       return providerInstance.isPrivate();
     }),
     getSignedUrl: wrapFunctionForErrors(async () => {
       return providerInstance.getSignedUrl();
-    })
-  })
+    }),
+  });
 
-  return { providerFunctions, providerOptions: p.options }
-}
+  return { providerFunctions, providerOptions: p.options };
+};
 
 module.exports = {
-  init (options) {
+  init(options) {
     return {
-      upload (file) {
+      upload(file) {
         try {
           const { providerFunctions, providerOptions } = getProviderData(
             file,
             options
-          )
+          );
           strapi.log.info("DEBUG - calling providerFunctions upload");
-          return providerFunctions.upload(file)
+          return providerFunctions.upload(file);
         } catch (err) {
           strapi.log.info("DEBUG - error occurred");
-          return null
+          return null;
         }
       },
       uploadStream(file) {
@@ -136,49 +139,49 @@ module.exports = {
           const { providerFunctions, providerOptions } = getProviderData(
             file,
             options
-          )
+          );
           strapi.log.info("DEBUG - calling providerFunctions uploadStream");
-          return providerFunctions.uploadStream(file)
+          return providerFunctions.uploadStream(file);
         } catch (err) {
           strapi.log.info("DEBUG - error occurred");
-          return null
+          return null;
         }
       },
-      delete (file) {
+      delete(file) {
         try {
           const { providerFunctions, providerOptions } = getProviderData(
             file,
             options
-          )
-          return providerFunctions.delete(file)
+          );
+          return providerFunctions.delete(file);
         } catch (err) {
-          return null
+          return null;
         }
       },
-      isPrivate (file) {
+      isPrivate(file) {
         try {
           const { providerFunctions, providerOptions } = getProviderData(
             file,
             options
-          )
+          );
           strapi.log.info("DEBUG - calling providerFunctions isPrivate");
           return providerFunctions.isPrivate();
         } catch (err) {
-          return null
+          return null;
         }
       },
-      getSignedUrl (file) {
+      getSignedUrl(file) {
         try {
           const { providerFunctions, providerOptions } = getProviderData(
             file,
             options
-          )
+          );
           strapi.log.info("DEBUG - calling providerFunctions getSignedUrl");
           return providerFunctions.getSignedUrl();
         } catch (err) {
-          return null
+          return null;
         }
-      }
-    }
-  }
-}
+      },
+    };
+  },
+};
